e8bff2e1faa18fc83aab058d0b58d129ba89fa49
race-briggs <race.briggs@gmail.com>
2019-10-01 09:53:46 -0500

    added .env contents and spotify key

--- liri.js
+++ liri.js
@@ -1,26 +1,23 @@
 //require dependencies
 require('dotenv').config();
 
+var keys = require('./keys.js');
+
 var axios = require('axios');
 var moment = require('moment');
-var Spotify = require('node-spotify-api');
+var spotify = require(keys.spotify);
 
-var spotify = new Spotify({
-  id: 'a30d6bb0045c4ed38ff92f0f94b0cb6d',
-  secret: '05df9fa33bbd4a00a262b8bce66c9193'
-})
 
 function liriHelp(action){
   console.log('Hello silly human, LIRI is here to help you with your mundane mortal problems.')
   switch(action){
-    case 'find-movie':
+    case 'movie-this':
 
       break;
-    case 'find-song':
-      var searchType = process.argv[3];
-      var queryString = process.argv[4];
+    case 'spotify-this-song':
+      var queryString = process.argv[3];
 
-      spotify.search({type: searchType, query: queryString, limit: 5}, function(err, data){
+      spotify.search({ type: 'artist OR album OR track', query: queryString, limit: 1}, function(err, data){
         if(err){
           return console.log(err)
         }
@@ -28,7 +25,10 @@ function liriHelp(action){
         console.log(JSON.stringify(data, null, 2));
       })
       break;
-    case 'find-concert':
+    case 'concert-this':
+
+      break;
+    case 'do-what-it-says':
 
       break;
   }
